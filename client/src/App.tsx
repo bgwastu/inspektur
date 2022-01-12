cc8d84004a39cb75a6beac647728f94103ba818d
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-01-12 23:54:56 +0700

    Cleaning and add necessary dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+function App() {
+    return (
+        <Typography variant="h1">
+            Hello World!
+        </Typography>
+    );
+}
+
+export default App;
